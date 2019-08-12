d222ef35eaeb046196ba213c1c00b95cf488a31f
minhtuan2000 <minhtuan270820000@gmail.com>
2019-08-12 21:33:28 +0000

    update

--- Extension/autoplay.js
+++ Extension/autoplay.js
@@ -31,19 +31,27 @@ function autoPlay(apID, id, url, i, highlights, isBasic, durations){
     //Check if autoPlay should continue
     if (apID != autoplayID) return;
     // Return after playing every highlights
-    let autoplayButton = document.getElementById("autoplay-button");
+    const autoplayButton = document.getElementById("autoplay-button");
+    const quitButton = document.getElementById("quit-button");
     if (i >= highlights.length){
         let autoplayWarning = document.getElementById("autoplay-warning");
         autoplayWarning.style.display = "none";
         autoplayButton.textContent = "Autoplay";
         setAutoplayButton(id, url, highlights, isBasic, durations);
         //Reset all highlights buttons
-        // Remove old buttons
-        removeOldButtons();
-        // Add new buttons
-        for (let i = 0; i < highlights.length; i++){
-          setButton(id, url, i, highlights[i]);
+        buttonList = document.getElementsByClassName("button");
+        for (let i = 0; i < buttonList.length; i++){
+            try{
+                buttonList[i].onclick = function(){    
+                    buttonList[i].style.backgroundColor = "darkorange";
+                    chrome.tabs.update(id, {url: "https://www.twitch.tv/videos/" + getVideoCode(url) + "?t=" + highlights[i]});   
+                }
+            } catch (err){
+                console.log(err);
+            }
         }
+        //Reset quit button
+        quitButton.style.display = "none";
     } else {
         chrome.tabs.update(id, {url: "https://www.twitch.tv/videos/" + getVideoCode(url) + "?t=" + highlights[i]});
         document.getElementById((i + 1).toString()).style.backgroundColor = "darkorange";
@@ -53,6 +61,13 @@ function autoPlay(apID, id, url, i, highlights, isBasic, durations){
             autoplayID += 1;
             autoPlay(autoplayID, id, url, i + 1, highlights, isBasic, durations);
         }
+
+        quitButton.style.display = "inline-block";
+        quitButton.onclick = function(){
+            autoplayID += 1;
+            autoPlay(autoplayID, id, url, highlights.length , highlights, isBasic, durations);
+        }
+
         if (isBasic){
             setTimeout(() => {autoPlay(apID, id, url, i + 1, highlights, isBasic, durations)}, l * 60 * 1000);
         } else {
